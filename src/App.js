6e49b6f0b74513c29b26a3b171da5d6ee7c0b2fe
imstreetspirit <imstreetspirit@users.noreply.github.com>
2020-06-02 01:46:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+import Notepad from './Notepad';
+
+class App extends PureComponent {
+
+  render() {
+    return (
+      <div> <Notepad></Notepad>
+      </div>      
+
+    )
+  }
+}
+
+export default App;
